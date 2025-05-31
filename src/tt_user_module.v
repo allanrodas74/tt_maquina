d8435b6b81cf7b49ff5241ff29cf45dbb612a35d
allanrodas74 <80084449+allanrodas74@users.noreply.github.com>
2025-05-31 05:29:43 -0600

    Update tt_user_module.v

--- src/tt_user_module.v
+++ src/tt_user_module.v
@@ -4,7 +4,7 @@ module tt_um_allanrodas74 (
     output wire [7:0] io_out,  // io_out[6:0] = {R1,N1,P1,D4,D3,D2,D1}
     input wire ena,
     input  wire       clk,     // reloj global Tiny Tapeout
-    input  wire       rst      // reset global
+    input  wire       rst_n      // reset global
 );
     wire [3:0] sw = io_in[3:0];   // P,R,N,D
     wire [6:0] led;
