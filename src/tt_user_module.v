c456cba11c14ef338e2156bc0ece84642a0220c3
allanrodas74 <80084449+allanrodas74@users.noreply.github.com>
2025-05-31 05:52:43 -0600

    Update tt_user_module.v

--- src/tt_user_module.v
+++ src/tt_user_module.v
@@ -1,16 +1,15 @@
 module tt_um_allanrodas74 (
-    input  wire [7:0] ui_in,
-    output wire [7:0] uo_out,
-    input  wire [7:0] uio_in,
-    output wire [7:0] uio_out,
-    output wire [7:0] uio_oe,
-    input  wire       ena,
-    input  wire       clk,
-    input  wire       rst_n
+    input  wire [7:0] ui_in,       // Entradas estándar
+    output wire [7:0] uo_out,      // Salidas estándar
+    input  wire [7:0] uio_in,      // Entradas bidireccionales
+    output wire [7:0] uio_out,     // Salidas bidireccionales
+    output wire [7:0] uio_oe,      // Control de salida (output enable)
+    input  wire       ena,         // Enable de usuario
+    input  wire       clk,         // Reloj global
+    input  wire       rst_n        // Reset global activo bajo
 );
 
-    // Entradas P, R, N, D conectadas a ui_in[6:3]
-    wire [3:0] sw = ui_in[6:3];
+    wire [3:0] sw = ui_in[6:3];    // P, R, N, D
     wire [6:0] led;
 
     tt_Maquina_Top top (
@@ -20,8 +19,8 @@ module tt_um_allanrodas74 (
         .led   (led)
     );
 
-    assign uo_out = {1'b0, led};   // LED en bits 6:0
-    assign uio_out = 8'b0;         // No se usan los I/O bidireccionales
-    assign uio_oe  = 8'b0;         // Se deshabilitan (alta impedancia)
+    assign uo_out  = {1'b0, led}; // LED en bits [6:0]
+    assign uio_out = 8'b0;        // No usas uio, se mantiene en 0
+    assign uio_oe  = 8'b0;        // Salidas en alta impedancia
 
 endmodule
